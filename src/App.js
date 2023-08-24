5f857c31240af3bbe31729cb55119dbc25bae32f
Samiksha <swabhimankhadka2001@gmail.com>
2023-08-24 11:45:06 +0545

    tailwind simple update

---
+++ src/App.js
@@ -0,0 +1,237 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+
+    <div className="h-screen cursor-not-allowed">
+    <div className="bg-white mx-auto max-w-4xl p-10 border border-gray-300">
+      <p className="text-center text-lg font-bold">SAMIKSHA KHADKA</p>
+      <div className="text-center text-lg font-medium">
+        <a href="https://www.linkedin.com/in/samiksha-khadka-865b281a1/">LinkedIn</a> | <a href="#">Github</a> | <a href="#">Medium</a><br />
+        Email: swabhimankhadka2001@gmail.com | Phone: 9861172978
+      </div>
+
+      <div className="mt-2">
+        <section className="">
+          <h2 className="text-2xl font-semibold ">ABOUT ME</h2>
+          <hr className=" border-black mb-0 mt-0 my-5" />
+          <p font-medium mt-0>Due to my naturally curious nature, I find myself drawn to a wide array of dimensions, ranging from computers and technology to economics and entrepreneurship, along with other captivating subjects such as music, politics, and mathematics. I'm not only chatty but also known for adding humor to my interactions.</p>
+        </section>
+      </div>
+
+      <div className='mt-2'>
+        <section className="mb-2 ">
+          <h2 className='text-2xl font-semibold'>EDUCATION</h2>
+          <hr className=" border-black mt-0 my-5 mb-0" />
+          <p className='font-bold'>Tribhuwan University, Kathmandu</p>
+          <p>Padma Kanya Multiple Campus, Bagbazzar <br />
+            Bachelor of Science in Computer Science and Information Technology <br />
+            3rd year/ 5th sem
+          </p>
+        </section>
+
+
+      </div>
+
+      <div>
+        <section className='mb-2'>
+          <h2 className='text-2xl font-semibold'> EXPERIENCE </h2>
+          <hr className=" border-black mt-0 my-5 mb-0" />
+          <h4 className="text-xl font-semibold">Solution Developer → <a href="https://inspireuvic.org/" className="text-blue-500">INSPIRE Uvic</a> (Aug 21, 2023 – present)</h4>
+          <ul className="list-disc pl-6 mt-2">
+            <li className="ml-4">INSPIRE: STEM for Social Impact at UVic, work closely with community, industry, and research partners, to solve real-world problems within local communities.</li>
+          </ul>
+        </section>
+      </div>
+
+      <div className="mt-2">
+        <section className="mb-2">
+          <div className="flex items-center">
+            <h4 className="text-xl font-semibold">Co-founder → <a href="https://www.facebook.com/0thealgorithm" className='text-blue-500'>THE ALGORITHM</a> (2023)</h4>
+          </div>
+          <ul className="list-disc pl-6 mt-2">
+            <li className="ml-4">
+              An informal group/club founded in Padma Kanya Multiple campus for the promotion of the use of technology for strengthening democracy and gender equality.
+            </li>
+          </ul>
+        </section>
+      </div>
+
+
+      <div className="mt-2">
+        <section className="mb-2">
+
+          <h4 className="text-xl font-semibold">Research Assistant to hon MP → <a href="https://pokharacentre.org/ " className='text-blue-500'>Policy and Governance Fellow at Province 1 </a> (2022 November – 2023 October)</h4>
+
+          <ul className="list-disc pl-6 mt-2">
+            <li className="ml-4">
+              A fellowship program by Pokhara Research Lab, funded by US Embassy for youth participation in strengthening democracy.
+            </li>
+          </ul>
+          <div className="pl-6 mt-2">
+            <h4 className="font-semibold">Responsibilities</h4>
+            <ul className="list-disc pl-6 mt-2">
+              <li className="ml-4">
+                Attend 3 residential training camps to better understand the functioning of Nepal government and policy making.
+              </li>
+              <li className="ml-4">
+                Attend a certification course on ‘Legislative Research’.
+              </li>
+            </ul>
+          </div>
+        </section>
+      </div>
+
+
+      <div className="mt-2">
+        <section className="mb-2">
+          <div className="flex items-center">
+            <h4 className="text-xl font-semibold">Intern → <a href="https://usefnepal.org/?fbclid=IwAR1OkVsjPdVRPQDkt5N-OW77U5AoaihQq5xPnHMypp4-v7ydRfU_GbbUUuc" className='text-blue-500'>Fulbright Nepal </a> (2022 Nov 14 - 2023 March 14)</h4>
+          </div>
+          <ul className="list-disc pl-6 mt-2">
+            <li className="ml-4">
+              Communication and Administration Intern
+            </li>
+            <li className="ml-4">
+              The Fulbright Commission in Nepal, administers the U.S.-funded Fulbright Program. Working in conjunction with the U.S. Embassy, Nepal.
+            </li>
+          </ul>
+          <div className="pl-6 mt-4">
+            <h4 className="font-semibold">Responsibilities</h4>
+            <ul className="list-disc pl-6 mt-2">
+              <li className="ml-4">
+                Outreach to alumni and partner organizations
+              </li>
+              <li className="ml-4">
+                Social media and event logistics handling
+              </li>
+              <li className="ml-4">
+                Assist in the selection process for various scholarship initiatives.
+              </li>
+            </ul>
+          </div>
+        </section>
+      </div>
+
+      <div className="mt-6">
+
+        <div className="section-title">
+          <h2 className='text-2xl font-semibold'>PROJECTS</h2>
+
+          <hr className="border-t mt-0 border-black my-5 mb-0" />
+
+
+          <h3 className='font-semibold mb-0 my-1'> <a href="https://github.com/samikshakhadka/Indian_Unicorn_Startup_anlysis " className='text-blue-500'>Unicorn Startup Analysis</a></h3>
+
+          <ul className="list-disc pl-6 mt-0 ">
+            <li className="ml-4 mt-0 my-0">
+              An analysis on Indian Unicorn Startup companies.
+            </li>
+            <li className="ml-4 ">
+              Using statistical analysis, Python programming, and data visualization techniques.
+            </li>
+          </ul>
+        </div>
+      </div>
+
+
+      <div className="mt-2">
+        <div className="section">
+          <div className="section-title">
+            <h2 className='text-2xl font-semibold '>CERTIFICATIONS</h2>
+          </div>
+          <hr className="border-t mt-0 border-black my-5 mb-0" />
+
+          <div className="experience-item">
+            <ul className="list-decimal pl-6 mt-2">
+              <li className="ml-4">
+                Introduction to SQL <a href="LINK">LINK</a>
+              </li>
+              <li className="ml-4">
+                Intermediate SQL <a href="LINK">LINK</a>
+              </li>
+              <li className="ml-4">
+                Understanding Data Engineering <a href="LINK">LINK</a>
+              </li>
+              <li className="ml-4">
+                Introduction to Statistics in Python <a href="LINK">LINK</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div className='mt-2'>
+        <h3 className='text-2xl font-semibold'>ACHIEVEMENTS</h3>
+        <hr className='border-t mt-0 border-black my-5 mb-0'></hr>
+        <ul className='list-disc pl-6 mt-2'>
+          <li className='ml-4'>
+            Top 6 in Nepal’s largest All Female Hacking for Humanity Hackathon
+          </li>
+
+          <li className='ml-4'>
+            MIT Policy Hackathon participant.
+          </li>
+
+          <li className='ml-4'>
+            Hult Prize 2022 Oncamus winner
+
+          </li>
+
+          <li className='ml-4'>
+            Microsoft Imagine Cup 2022 SE Asia finalist.
+          </li>
+        </ul>
+      </div>
+
+
+      <div className='mt-2'>
+        <h2 className='text-2xl font-semibold'>SCHOLARSHIPS</h2>
+        <hr className='border-t mt-0 mb-1 border-black my-5'></hr>
+        <h3 className='font-semibold'></h3>
+
+        <ul className='list-disc pl-6 mt-0 mb-0'>
+          <li className='ml-4'>
+            Jyoti Scholarship Award by Zonta Club (Technology Category)
+          </li>
+
+          <li className='ml-4'>
+            Grace Hopper Conference Scholar 2022
+          </li>
+
+          <li className='ml-4'>
+            Data Camp X Smart Cheli scholarship for data science.
+
+          </li>
+
+
+          <li className='ml-4'>
+            Research Incubation Award winner(2021) provided by Brainycubes Research Lab.
+
+
+          </li>
+
+        </ul>
+
+      </div>
+
+      <button className="bg-blue-500 text-white py-2 px-4 rounded pointer-events-none">
+        Non-Interactive Button
+      </button>
+
+
+
+
+
+
+
+
+
+
+    </div>
+    </div>
+  );
+}
+
+export default App;
